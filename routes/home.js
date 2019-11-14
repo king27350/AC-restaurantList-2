2fbd18bdc813cdabcd552230be90822cd35d6e40
Chris <shian0222@gmail.com>
2019-11-14 21:26:09 +0800

    Add coupon feature and modal

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,11 @@ const Restaurant = require('../models/restaurant')
 // 餐廳首頁
 router.get('/', (req, res) => {
   Restaurant.find((err, restaurants) => {
+    //從資料庫隨機取1組餐廳
+    const rnd = Math.floor(Math.random() * Math.floor(restaurants.length))
+    const coupon = Math.random().toString(36).substr(2)
     if (err) return console.error(err)
-    return res.render('index', { restaurants: restaurants })
+    return res.render('index', { restaurants: restaurants, randomRestaurant: restaurants[rnd], coupon })
   })
 })
 
