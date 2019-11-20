6a7a1cc85d6bf004618ac71d9abb024d48585c9e
Chris <shian0222@gmail.com>
2019-11-20 21:16:47 +0800

    Add user registration feature

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new Schema({
     required: true
   },
   date: {
-    type: Date(),
+    type: Date,
     default: Date.now()
   }
 })
--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,30 @@ router.get('/register', (req, res) => {
 
 // register inspection
 router.post('/register', (req, res) => {
-  res.send('logout')
+  const { name, email, password, password2 } = req.body
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      console.log('User email already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/')
+        })
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 module.exports = router
